48eca4d623e9481328d0131098d92397bf5f80d5
MarcoAlayn <marc_rob92@hotmail.com>
2023-09-14 12:21:18 -0700

    Added the functionality to add Pokémon to favorites in DB

--- web-app/src/components/pokemonCard/PokemonCard.js
+++ web-app/src/components/pokemonCard/PokemonCard.js
@@ -1,9 +1,11 @@
 import React from "react";
+import { useDispatch, useSelector } from "react-redux";
 import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
 import Typography from "@mui/material/Typography";
 import CardMedia from "@mui/material/CardMedia";
 import { styled } from "@mui/material/styles";
+import { addToFavorites } from "../../redux/actions/PokemonActions";
 
 const StyledCard = styled(Card)(({ theme }) => ({
   maxWidth: 300,
@@ -19,8 +21,15 @@ const StyledCardMedia = styled(CardMedia)(({ theme }) => ({
 }));
 
 const PokemonCard = ({ id, name, imageFront, imageBack }) => {
+  const dispatch = useDispatch();
+  const userId = useSelector((state) => state.user.user.id);
+  const handleCardClick = () => {
+    console.log(id, userId)
+    dispatch(addToFavorites(id, userId));
+  };
+
   return (
-    <StyledCard>
+    <StyledCard onClick={handleCardClick}>
       <StyledCardMedia image={imageFront} title={name} />
       <CardContent>
         <Typography variant="h5" component="div">
